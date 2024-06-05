2a26dd360698ee74f9c92b3f08bb98e47284f832
Bùi Huỳnh Quốc AnH <117139949+bhqanhuit@users.noreply.github.com>
2024-06-05 13:13:04 +0700

    update inference
    update inference

--- main.py
+++ main.py
@@ -17,18 +17,13 @@ from utils.image_utils import display_image_list, process_display_input
 from utils.model_utils import load_base_models
 from utils.options import Options
 from torchvision.utils import save_image
+import torchvision.transforms as T
 import time
 
 opts = Options().parse()
 
-
-
-
-
-
-
 def hairstyle_editing(global_cond=None, local_sketch=False, paint_the_mask=False, \
-                      src_latent=src_latent, src_feature=src_feature, input_mask=input_mask, src_image=src_image, \
+                      src_latent=None, src_feature=None, input_mask=None, src_image=None, \
                         latent_global=None, latent_local=None, latent_bald=None, local_blending_mask=None, painted_mask=None):
     if paint_the_mask:
         modified_mask = painting_mask(input_mask)
@@ -64,41 +59,51 @@ def hairstyle_editing(global_cond=None, local_sketch=False, paint_the_mask=False
 
 # print("--- %s seconds ---" % (time.time() - start_time))
 
-if (__name__ == '__main__'):
-    with open('datasets/testPair.txt') as file:
-        lines = [line.rstrip() for line in file]
-        cnt = 0
-        for line in lines:
-            cnt += 1
-            source, shape = line.split(' ')
-            print(source, shape)
 
-            if (cnt > 5): break
-            src_name = '08255'
-            
-            image_transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
-            g_ema, mean_latent_code, seg = load_base_models(opts)
-            ii2s = Embedding(opts, g_ema, mean_latent_code[0,0])
+g_ema, mean_latent_code, seg = load_base_models(opts)
+ii2s = Embedding(opts, g_ema, mean_latent_code[0,0])
+
+bald_proxy = BaldProxy(g_ema, opts.bald_path)
+text_proxy = TextProxy(opts, g_ema, seg, mean_latent_code)
+ref_proxy = RefProxy(opts, g_ema, seg, ii2s)
+sketch_proxy = SketchProxy(g_ema, mean_latent_code, opts.sketch_path)
+color_proxy = ColorProxy(opts, g_ema, seg)
 
 
-            if not os.path.isfile(os.path.join(opts.src_latent_dir, f"{src_name}.npz")):
-                inverted_latent_w_plus, inverted_latent_F = ii2s.invert_image_in_FS(image_path=f'{opts.src_img_dir}/{src_name}.jpg')
-                save_latent_path = os.path.join(opts.src_latent_dir, f'{src_name}.npz')
-                np.savez(save_latent_path, latent_in=inverted_latent_w_plus.detach().cpu().numpy(),
-                            latent_F=inverted_latent_F.detach().cpu().numpy())
-            src_latent = torch.from_numpy(np.load(f'{opts.src_latent_dir}/{src_name}.npz')['latent_in']).cuda()
-            src_feature = torch.from_numpy(np.load(f'{opts.src_latent_dir}/{src_name}.npz')['latent_F']).cuda()
-            src_image = image_transform(Image.open(f'{opts.src_img_dir}/{src_name}.jpg').convert('RGB')).unsqueeze(0).cuda()
-            input_mask = torch.argmax(seg(src_image)[1], dim=1).long().clone().detach()
 
-            bald_proxy = BaldProxy(g_ema, opts.bald_path)
-            text_proxy = TextProxy(opts, g_ema, seg, mean_latent_code)
-            ref_proxy = RefProxy(opts, g_ema, seg, ii2s)
-            sketch_proxy = SketchProxy(g_ema, mean_latent_code, opts.sketch_path)
-            color_proxy = ColorProxy(opts, g_ema, seg)
+with open('datasets/testPair.txt') as file:
+    lines = [line.rstrip() for line in file]
+    cnt = 0
+    for line in lines:
+        cnt += 1
+        source, shape = line.split(' ')
+        print(source, shape)
+    
+        src_name = source.split('.')[0]
+        transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+        source_im = transform(Image.open(f'{opts.src_img_dir}/{src_name}.png')).to('cuda')
+        shape_im = transform(Image.open(f'{opts.ref_img_dir}/{shape}')).to('cuda')
 
-            edited_hairstyle_img = src_image
+        image_transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
 
-            start_time = time.time()
+        if not os.path.isfile(os.path.join(opts.src_latent_dir, f"{src_name}.npz")):
+            inverted_latent_w_plus, inverted_latent_F = ii2s.invert_image_in_FS(image_path=f'{opts.src_img_dir}/{src_name}.png')
+            save_latent_path = os.path.join(opts.src_latent_dir, f'{src_name}.npz')
+            np.savez(save_latent_path, latent_in=inverted_latent_w_plus.detach().cpu().numpy(),
+                        latent_F=inverted_latent_F.detach().cpu().numpy())
+        src_latent = torch.from_numpy(np.load(f'{opts.src_latent_dir}/{src_name}.npz')['latent_in']).cuda()
+        src_feature = torch.from_numpy(np.load(f'{opts.src_latent_dir}/{src_name}.npz')['latent_F']).cuda()
+        src_image = image_transform(Image.open(f'{opts.src_img_dir}/{src_name}.png').convert('RGB')).unsqueeze(0).cuda()
+        input_mask = torch.argmax(seg(src_image)[1], dim=1).long().clone().detach()
+
+        
+        edited_hairstyle_img = src_image
+        src_feature, edited_hairstyle_img = hairstyle_editing(global_cond=shape, local_sketch=False, paint_the_mask=False, src_latent=src_latent, src_feature=src_feature, input_mask=input_mask, src_image=src_image)
+
+        start_time = time.time()
+        print("--- %s seconds ---" % (time.time() - start_time))
+        save_image(edited_hairstyle_img.squeeze(), 'test_outputs/' + str(cnt).zfill(10) + '.jpg', normalize=True)
+        save_image(torch.cat([edited_hairstyle_img.squeeze(), source_im, shape_im], dim=2), 'test_outputsFull/' + str(cnt).zfill(10) + '.png', normalize=True)
+        # if (cnt > 5): break
 
             
